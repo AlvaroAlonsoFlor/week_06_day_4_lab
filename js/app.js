b9d6204b7bf671596390afb22563874ed639a4a7
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-06 14:39:06 +0100

    Add querySelector and start handleFormSumbit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
 })
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  console.log(event.target.title);
+};
