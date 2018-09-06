27061c70c29de2b17e29008ac7f4f09cf8119b45
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-06 15:28:37 +0100

    Add submit new objects without overriding

--- js/app.js
+++ js/app.js
@@ -6,11 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
-  // console.log(event.target.title.value);
-  // console.log(event.target.author.value);
-  // console.log(event.target.category.value);
   const output = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`;
-  const resultList =  document.querySelector('#reading-list');
+  // const resultList =  document.querySelector('#reading-list');
+  const newElement = document.createElement('p')
+  newElement.textContent = output
+
+  const readingList = document.querySelector('#reading-list');
+  readingList.appendChild(newElement)
+
+
+  // resultList.textContent = output;
+
+
+ //reset
+  const form = document.querySelector('#new-item-form').reset()
+
+
   console.log(resultList);
-  resultList.textContent = output;
 };
