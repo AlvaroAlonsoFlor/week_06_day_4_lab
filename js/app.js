2334e60678531104ea15c74c05370e051c8a8d79
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-06 15:38:22 +0100

    Finish delete button

--- js/app.js
+++ js/app.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
+
+  const remove = document.querySelector('#delete');
+  remove.addEventListener('click', handleRemoveClick)
 })
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
   const output = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`;
-  // const resultList =  document.querySelector('#reading-list');
+
   const newElement = document.createElement('p')
   newElement.textContent = output
 
@@ -15,12 +18,14 @@ const handleFormSubmit = function (event) {
   readingList.appendChild(newElement)
 
 
-  // resultList.textContent = output;
-
-
  //reset
   const form = document.querySelector('#new-item-form').reset()
 
 
   console.log(resultList);
+};
+
+const handleRemoveClick = function (event) {
+  const readingList = document.querySelector('#reading-list')
+  readingList.textContent = ""
 };
